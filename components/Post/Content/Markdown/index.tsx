8a3cba93d14fd365af0d5a722735e6609a009fa4
unknown <dlord4242@gmail.com>
2022-03-08 16:49:09 +0900

    Adjusted styles and added features of react-markdown & syntax-highlighter

--- components/Post/Content/Markdown/index.tsx
+++ components/Post/Content/Markdown/index.tsx
@@ -1,14 +1,32 @@
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import { useContext } from 'react';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
-import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
-import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 
+import darkTheme from 'react-syntax-highlighter/dist/cjs/styles/prism/vs-dark';
+import lightTheme from 'react-syntax-highlighter/dist/cjs/styles/prism/prism';
+
+import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript';
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
+import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
+import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss';
+import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json';
+import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash';
+
+import { AppContext } from 'context/AppContext';
 import classes from './index.module.scss';
 
-SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('javascript', javascript);
+SyntaxHighlighter.registerLanguage('typescript', typescript);
+SyntaxHighlighter.registerLanguage('jsx', jsx);
+SyntaxHighlighter.registerLanguage('tsx', tsx);
 SyntaxHighlighter.registerLanguage('css', css);
+SyntaxHighlighter.registerLanguage('scss', scss);
+SyntaxHighlighter.registerLanguage('json', json);
+SyntaxHighlighter.registerLanguage('bash', bash);
 
 interface MarkdownProps {
   slug: string;
@@ -16,9 +34,14 @@ interface MarkdownProps {
 }
 
 const Markdown: React.FC<MarkdownProps> = ({ slug, content }) => {
+  const { theme } = useContext(AppContext);
+
+  const colorTheme = theme === 'light' ? lightTheme : darkTheme;
+
   return (
     <section className={classes.markdown}>
       <ReactMarkdown
+        rehypePlugins={[rehypeRaw]}
         components={{
           p: ({ node, children }: any) => {
             if (node.children[0].tagName === 'img') {
@@ -38,13 +61,32 @@ const Markdown: React.FC<MarkdownProps> = ({ slug, content }) => {
             return <p>{children}</p>;
           },
 
-          code: ({ className, children }) => {
+          code: ({ className, inline, children }) => {
             const language = (className || '').split('-')[1];
 
-            return (
-              <SyntaxHighlighter style={atomDark} language={language}>
+            return !inline ? (
+              <SyntaxHighlighter
+                style={colorTheme}
+                language={language}
+                customStyle={{
+                  padding: '2rem 2rem 0 2rem',
+                  fontSize: '1.5rem',
+                  borderRadius: '5px',
+                }}
+              >
                 {children}
               </SyntaxHighlighter>
+            ) : (
+              <code
+                style={{
+                  ...colorTheme['pre[class*="language-"]'],
+                  padding: '0.25rem 0.5rem',
+                  fontSize: '1.5rem',
+                  borderRadius: '5px',
+                }}
+              >
+                {children}
+              </code>
             );
           },
         }}
